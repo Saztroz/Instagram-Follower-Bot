ac3635dc5f72998c2e989a2424e505a13b8f4390
Hussein <67486007+Saztroz@users.noreply.github.com>
2021-05-07 14:28:07 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import ElementClickInterceptedException
 import time
 
-CHROME_DRIVER_PATH = YOUR CHROM DRIVER PATH
+CHROME_DRIVER_PATH = "D:\Development\chromedriver.exe"
 SIMILAR_ACCOUNT = "buzzfeedtasty"
 USERNAME = YOUR INSTAGRAM USERNAME
 PASSWORD = YOUR INSTAGRAM PASSWORD
